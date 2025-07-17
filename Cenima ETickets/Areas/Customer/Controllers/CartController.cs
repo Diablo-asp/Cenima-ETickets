19f71d11794dd0f135e5e1d12b0fdb907a852380
Jossef Saied <Di0ab0lo0@Gmail.com>
2025-07-18 00:41:47 +0300

    Add Pay Action to Cart Add Success Action to Checkout Controller

--- Cenima ETickets/Areas/Customer/Controllers/CartController.cs
+++ Cenima ETickets/Areas/Customer/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 
 namespace Cinema_ETickets.Areas.Customer.Controllers
 {
@@ -12,13 +13,15 @@ namespace Cinema_ETickets.Areas.Customer.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ICartRepository _cartRepository;
         private readonly ApplicationDbContext _context;
+        private readonly IOrderRepository _orderRepository;
 
         public CartController(UserManager<ApplicationUser> userManager, ICartRepository cartRepository,
-            ApplicationDbContext context)
+            ApplicationDbContext context,IOrderRepository orderRepository)
         {
             _userManager = userManager;
             _cartRepository = cartRepository;
             _context = context;
+            _orderRepository = orderRepository;
         }
 
         public async Task<IActionResult> AddToCart(int MovieId, int Count)
@@ -137,6 +140,73 @@ namespace Cinema_ETickets.Areas.Customer.Controllers
                 return NotFound();
             }
             return NotFound();
+
+        }
+
+        public async Task<IActionResult> Pay()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user is not null)
+            {
+                var carts = await _cartRepository.GetAsync(e => e.ApplicationUserId == user.Id, includes: [e => e.Movie]);
+
+                if (carts is not null)
+                {
+
+                    await _orderRepository.CreateAsync(new()
+                    {
+                        ApplicationUserId = user.Id,
+                        Date = DateTime.UtcNow,
+                        OrderStatus = OrderStatus.pending,
+                        PaymentMethod = PaymentMethod.Visa,
+                        TotalPrice = carts.Sum(e => e.Movie.Price * e.Count)
+                    });
+
+                    var order = (await _orderRepository.GetAsync(e => e.ApplicationUserId == user.Id)).OrderBy(e => e.Id).LastOrDefault();
+
+                    if (order is null)
+                        return BadRequest();
+
+                    var options = new SessionCreateOptions
+                    {
+                        PaymentMethodTypes = new List<string> { "card" },
+                        LineItems = new List<SessionLineItemOptions>(),
+                        Mode = "payment",
+                        SuccessUrl = $"{Request.Scheme}://{Request.Host}/Customer/Checkout/Success?orderId={order.Id}",
+                        CancelUrl = $"{Request.Scheme}://{Request.Host}/Customer/Checkout/Cancel",
+                    };
+
+
+                    foreach (var item in carts)
+                    {
+                        options.LineItems.Add(new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                Currency = "egp",
+                                ProductData = new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = item.Movie.Name,
+                                    Description = item.Movie.Description,
+                                },
+                                UnitAmount = (long)item.Movie.Price * 100,
+                            },
+                            Quantity = item.Count,
+                        });
+                    }
+
+
+                    var service = new SessionService();
+                    var session = service.Create(options);
+                    order.SessionId = session.Id;
+                    await _orderRepository.CommitAsync();
+                    return Redirect(session.Url);
+
+                }
+                return NotFound();
+            }
+            return NotFound();
+
 
         }
     }
---
+++ Cenima ETickets/Areas/Customer/Controllers/CheckoutController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+
+namespace Cinema_ETickets.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ICartRepository _cartRepository;
+        private readonly IOrderItemRepository _orderItemRepository;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CheckoutController(IOrderRepository orderRepository, ICartRepository cartRepository, IOrderItemRepository orderItemRepository, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _orderRepository = orderRepository;
+            _cartRepository = cartRepository;
+            _orderItemRepository = orderItemRepository;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Success(int orderId)
+        {
+            var order = await _orderRepository.GetOneAsync(e => e.Id == orderId);
+
+            if (order is null)
+                return NotFound();
+
+            order.OrderStatus = OrderStatus.Completed;
+
+            var service = new SessionService();
+            var session = service.Get(order.SessionId);
+
+            order.PaymentId = session.PaymentIntentId;
+
+            await _orderRepository.CommitAsync();
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user is null)
+                return NotFound();
+
+            var carts = await _cartRepository.GetAsync(e => e.ApplicationUserId == user.Id, includes: [e => e.Movie]);
+
+            List<OrderItem> orderItems = new();
+            foreach (var item in carts)
+            {
+                orderItems.Add(new()
+                {
+                    MovieId = item.MovieId,
+                    OrderId = orderId,
+                    Quantity = item.Count,
+                    TotalPrice = (decimal)(item.Movie.Price * item.Count)
+                });
+                var movie = _context.movies.Find(item.MovieId);
+                if(movie is not null)
+                {
+                    movie.TicketsQuantity -= item.Count;
+                }
+            }
+
+            await _orderItemRepository.CreateRangeAsync(orderItems);
+
+            foreach (var item in carts)
+            {
+                await _cartRepository.DeleteAsync(item);
+            }
+
+            await _context.SaveChangesAsync();
+
+            var orders = await _orderItemRepository.GetAsync(
+                e => e.OrderId == orderId,
+                includes: [e => e.Movie]);
+
+            return View(new SuccessVM
+            {
+                Order = order,
+                OrderItems = orderItems.ToList()
+            });
+        }
+    }
+}
